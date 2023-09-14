be439402f9f0508b604d8f0b58bc73daff93e081
d3noDL <dino@adenogame.com>
2023-09-14 21:24:40 +0200

    Collision detection + Naming convs.

--- hello.cpp
+++ hello.cpp
@@ -3,20 +3,31 @@
 int main(){
     
     // window dimensions
-    int width{800};
-    int height{450};
+    int window_width{800};
+    int window_height{450};
     
-    InitWindow(width, height, "Cool Window");
+    InitWindow(window_width, window_height, "Cool Window");
 
     // circle coordinates
+    int circle_x{200};
+    int circle_y{200};
+    int circle_radius{25};
+    //circle edges
+    int l_circle_x{circle_x - circle_radius};
+    int r_circle_x{circle_x + circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
     
-    int circleX{200};
-    int circleY{200};
 
     // axe coordinates
-
-    int axeX{400};
-    int axeY{0};
+    int axe_x{400};
+    int axe_y{0};
+    int axe_length{50};
+    // axe edges
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_length};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_length};
 
     int direction{10};
 
@@ -26,22 +37,22 @@ int main(){
         ClearBackground(WHITE);
         // game logic begins
         
-        DrawCircle(circleX, circleY, 25, BLUE);
-        DrawRectangle(axeX, axeY, 50, 50, RED);
+        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+        DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
 
         // move the axe
-        axeY += direction;
+        axe_y += direction;
 
-        if(axeY > 450 || axeY < 0){
+        if(axe_y > window_height || axe_y < 0){
             direction = -direction;
         }
         
-        if (IsKeyDown(KEY_D) && circleX < 800){
-            circleX += 10;
+        if (IsKeyDown(KEY_D) && circle_x < window_width){
+            circle_x += 10;
         }
 
-        if (IsKeyDown(KEY_A) && circleX > 0){
-            circleX -= 10;
+        if (IsKeyDown(KEY_A) && circle_x > 0){
+            circle_x -= 10;
         }
 
         // game logic ends
