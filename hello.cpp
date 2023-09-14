141aa321e58b7715c400ea17f407e86920a1fcd8
d3noDL <dino@adenogame.com>
2023-09-14 18:34:29 +0200

    Initial upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <cstdio> // Part of the standard library, should be in <>
+#include "raylib.h" //Header file for external library (Requires .h and "")
+
+int main(){
+    int width = 800;
+    float root_beer = 1.99;
+    double cheese_burger = 5.99;
+    bool shouldHaveLunch = true;
+    
+    InitWindow(width, 600, "Cool Window");
+}
