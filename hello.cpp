30792b3a0848f2d5a3de636f27680f537d3eaf6e
d3noDL <dino@adenogame.com>
2023-09-14 21:33:02 +0200

    Else statement and drawing text

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
 #include "raylib.h" //Header file for external library (Requires .h and "")
 
-int main(){
+int main() {
     
     // window dimensions
     int window_width{800};
@@ -31,31 +31,41 @@ int main(){
 
     int direction{10};
 
+    bool collision_with_axe{false};
+
     SetTargetFPS(60);
-    while (!WindowShouldClose()){
+    while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(WHITE);
-        // game logic begins
         
-        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
-        DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
-
-        // move the axe
-        axe_y += direction;
-
-        if(axe_y > window_height || axe_y < 0){
-            direction = -direction;
-        }
         
-        if (IsKeyDown(KEY_D) && circle_x < window_width){
-            circle_x += 10;
+        if (collision_with_axe) {
+            DrawText("Game Over!", 400, 200, 20, RED);
+        } else {
+            // game logic begins
+
+            DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+            DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
+
+            // move the axe
+            axe_y += direction;
+
+            if(axe_y > window_height || axe_y < 0){
+                direction = -direction;
+            }
+        
+            if (IsKeyDown(KEY_D) && circle_x < window_width){
+                circle_x += 10;
+            }
+
+            if (IsKeyDown(KEY_A) && circle_x > 0){
+                circle_x -= 10;
+            }
+
+            // game logic ends
         }
 
-        if (IsKeyDown(KEY_A) && circle_x > 0){
-            circle_x -= 10;
-        }
-
-        // game logic ends
+        
         EndDrawing();
     }
 }
